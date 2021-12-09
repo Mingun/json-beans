4f2004b03c874753cf74795d01d7722f29ceb9a9
Mingun <alexander_sergey@mail.ru>
2021-12-09 21:48:05 +0500

    Не генерируем классы для примитивных типов, а вместо этого пользуемся рантаймом

--- codegen/src/main/java/org/json/beans/codegen/Generator.java
+++ codegen/src/main/java/org/json/beans/codegen/Generator.java
@@ -23,6 +23,7 @@
  */
 package org.json.beans.codegen;
 
+import com.helger.jcodemodel.AbstractJType;
 import com.helger.jcodemodel.EClassType;
 import com.helger.jcodemodel.IJClassContainer;
 import com.helger.jcodemodel.JClassAlreadyExistsException;
@@ -35,10 +36,17 @@ import java.io.File;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import net.jimblackler.jsonschemafriend.Schema;
 import net.jimblackler.jsonschemafriend.SchemaStore;
+import org.json.beans.JsonBoolean;
+import org.json.beans.JsonInteger;
+import org.json.beans.JsonNull;
+import org.json.beans.JsonNumber;
+import org.json.beans.JsonScalar;
+import org.json.beans.JsonString;
 
 /**
  *
@@ -82,8 +90,8 @@ public class Generator {
    *         схемы, т.к. все имена будут уникальными, а схема валидируется, прежде чем быть переданной
    *         генератору
    */
-  private JDefinedClass generateTopLevel(String className, Schema schema) throws JClassAlreadyExistsException {
-    final JDefinedClass clazz = generate(model.rootPackage(), className, false, schema);
+  private AbstractJType generateTopLevel(String className, Schema schema) throws JClassAlreadyExistsException {
+    final AbstractJType clazz = generate(model.rootPackage(), className, false, schema);
     return clazz;
   }
   /**
@@ -101,8 +109,18 @@ public class Generator {
    *         схемы, т.к. все имена будут уникальными, а схема валидируется, прежде чем быть переданной
    *         генератору
    */
-  private JDefinedClass generate(IJClassContainer<JDefinedClass> container, String baseClassName, boolean inner, Schema schema) throws JClassAlreadyExistsException {
+  private AbstractJType generate(IJClassContainer<JDefinedClass> container, String baseClassName, boolean inner, Schema schema) throws JClassAlreadyExistsException {
     final List<Object> enums = schema.getEnums();
+    final Collection<String> types = schema.getExplicitTypes();
+    // Если тип не является перечислением и содержит только один элемент, описывающий скаляр,
+    // либо два элемента, один из которых скаляр, а другой -- "null", то генерация класса не требуется,
+    // используем встроенный тип
+    if (enums == null && types != null) {
+      final Class<?> scalar = toScalar(types);
+      if (scalar != null) {
+        return model.ref(scalar);
+      }
+    }
 
     final JDefinedClass clazz = container._class(
         JMod.PUBLIC,
@@ -114,7 +132,7 @@ public class Generator {
     for (final Map.Entry<String, Schema> e : schema.getProperties().entrySet()) {
       final String prop = e.getKey();
       final Schema s = e.getValue();
-      final JDefinedClass cls;
+      final AbstractJType cls;
       // Если схема объявлена по-месту, то делаем вложенный класс
       if (s.getParent() == schema) {
         cls = generate(clazz, toTitleCase(prop), true, s);
@@ -188,5 +206,105 @@ public class Generator {
       sb.append(part.substring(0, 1).toUpperCase()).append(part.substring(1));
     }
     return sb.toString();
+  }
+  /**
+   * Возвращает класс, который следует использовать для ограничения, описываемым с помощью одиночного
+   * значения ключа {@code "type"}. Маппинг схем на типы:
+   * <table>
+   * <tr><th>Схемы</th><th>Класс</th></tr>
+   * <tr>
+   * <td><code><pre>
+   * { "type": "boolean" }
+   * { "type": ["boolean", "null"] }
+   * { "type": ["null", "boolean"] }
+   * </pre></code></td>
+   * <td>{@link JsonBoolean}</td>
+   * </tr>
+   * <tr>
+   * <td><code><pre>
+   * { "type": "string" }
+   * { "type": ["string", "null"] }
+   * { "type": ["null", "string"] }
+   * </pre></code></td>
+   * <td>{@link JsonString}</td>
+   * </tr>
+   * <tr>
+   * <td><code><pre>
+   * { "type": "number" }
+   * { "type": ["number", "null"] }
+   * { "type": ["null", "number"] }
+   * </pre></code></td>
+   * <td>{@link JsonNumber}</td>
+   * </tr>
+   * <tr>
+   * <td><code><pre>
+   * { "type": "integer" }
+   * { "type": ["integer", "null"] }
+   * { "type": ["null", "integer"] }
+   * </pre></code></td>
+   * <td>{@link JsonInteger}</td>
+   * </tr>
+   * </table>
+   * Для всего остального возвращается {@code null}
+   *
+   * @param typeы Значение свойсва {@code "type"} JSON Schema
+   *
+   * @return Один из {@link JsonScalar скалярных} типов, за исключением {@link JsonNull}
+   */
+  private static Class<? extends JsonScalar> toScalar(Collection<String> types) {
+    Class<? extends JsonScalar> scalar = null;
+    for (final String type : types) {
+      // null, добавленный к примитивному типу, не меняет тип примитивной обертки, поэтому просто
+      // пропускаем
+      if ("null".equals(type)) continue;
+
+      // Если тип уже найден, то мы во второй (и последней) итерации цикла. Так как мы дошли досюда,
+      // то в списке не "null", а это значит, что у нас примитив ("boolean", "string", "number" или
+      // "integer") вместе с каким-то другим типом -- то есть тип у нас тип "oneOf", поэтому выходим,
+      // сигнализируя, что у нас не скалар.
+      if (scalar != null) return null;
+
+      // Если это единственное значение (помимо опционального "null"), то это будет тип для
+      // представления данных схемы
+      scalar = typeToScalar(type);
+    }
+    return scalar;
+  }
+  /**
+   * Возвращает класс, который следует использовать для ограничения, описываемым с помощью одиночного
+   * значения ключа {@code "type"}. Маппинг типов JSON Schema на классы:
+   * <table>
+   * <tr><th>Тип</th><th>Класс</th></tr>
+   * <tr>
+   * <td>{@code "boolean"}</td>
+   * <td>{@link JsonBoolean}</td>
+   * </tr>
+   * <tr>
+   * <td>{@code "string"}</td>
+   * <td>{@link JsonString}</td>
+   * </tr>
+   * <tr>
+   * <td>{@code "number"}</td>
+   * <td>{@link JsonNumber}</td>
+   * </tr>
+   * <tr>
+   * <td>{@code "integer"}</td>
+   * <td>{@link JsonInteger}</td>
+   * </tr>
+   * </table>
+   * Для всего остального возвращается {@code null}
+   *
+   * @param type Значение свойсва {@code "type"} JSON Schema
+   *
+   * @return Один из {@link JsonScalar скалярных} типов, за исключением {@link JsonNull}
+   */
+  private static Class<? extends JsonScalar> typeToScalar(String type) {
+    switch (type) {
+      case "boolean": return JsonBoolean.class;
+      case "string":  return JsonString.class;
+      case "number":  return JsonNumber.class;
+      case "integer": return JsonInteger.class;
+    }
+    return null;
   }
 }
